14dc3911c7f00844618286f3e4001641ee44b079
Park Jinsu <jinypark1606@gmail.com>
2023-10-24 18:06:35 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1><a href='/'>WEB</a></h1>
       </header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/1">css</a></li>
+          <li><a href="/read/1">js</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
-  );
+  )
 }
 
 export default App;
